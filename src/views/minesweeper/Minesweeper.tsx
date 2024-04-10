b5ea732a60549fafb48aeecae2c305bead3a54a1
ksemilla <kevinsemilla@gmail.com>
2024-04-10 16:46:04 +1000

    Add minesweeper game

---
+++ src/views/minesweeper/Minesweeper.tsx
@@ -0,0 +1,3 @@
+export function MinesweeperPage() {
+  return <div>minesweeper</div>
+}
--- src/views/snake/Snake.tsx
+++ src/views/snake/Snake.tsx
@@ -83,7 +83,7 @@ function generateBoxes(n: number, foodPos: number[]): Box[][] {
 }
 
 export function SnakePage() {
-  const n = 100
+  const n = 50
   const [food, setFood] = useState([
     Math.floor(Math.random() * n),
     Math.floor(Math.random() * n),
